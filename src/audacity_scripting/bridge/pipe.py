7128712a9044bc759069ce2bbede6ec995e81dc4
Meir Gabay <unfor19@gmail.com>
2023-12-14 02:15:10 +0200

    open with newline

--- src/audacity_scripting/bridge/pipe.py
+++ src/audacity_scripting/bridge/pipe.py
@@ -75,9 +75,9 @@ def do_command(command, retry_max_count=20, sleep_seconds=0.001):
         sleep(sleep_seconds)
 
     logger.debug("-- Both pipes exist.  Good.")
-    TOFILE = open(TONAME, WRITE_MODE)
+    TOFILE = open(TONAME, WRITE_MODE, newline=EOL)
     logger.debug("-- File to write to has been opened")
-    FROMFILE = open(FROMNAME, READ_MODE)
+    FROMFILE = open(FROMNAME, READ_MODE, newline=EOL)
     logger.debug("-- File to read from has now been opened too\r\n")
     send_command(TOFILE, EOL, command, close=close)
     response = get_response(FROMFILE, EOL=EOL, close=close)
